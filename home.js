7dd83b9a5be897f9e08c789d53136d8dc4fc7d88
Murali Mullapudi <murali177228@gmail.com>
2020-07-11 06:39:43 +0200

    Added new files

---
+++ home.js
@@ -0,0 +1,11 @@
+
+
+var a = prompt("Enter a value");
+var a1 = parseInt(a);
+console.log("a value is : "+ a);
+var b = prompt("Enter b value");
+var b1 = parseInt(b);
+console.log("b value is : "+ b);
+var result = a1 + b1;
+console.log("Result is: "+ result);
+// document.getElementById('demo').innerHTML = result.toString();
